d5d686a747c3250b5ac34e854cf16bf3b6b9693a
ShakeLake <XX002XX015XX@yandex.ru>
2022-01-19 19:18:13 +0500

    default

---
+++ client/client.hpp
@@ -0,0 +1,12 @@
+#ifndef CLIENT_HPP_
+#define CLIENT_HPP_
+
+class Client
+{
+private:
+    
+public:
+
+};
+
+#endif /* CLIENT_HPP_ */
---
+++ server/server.hpp
@@ -0,0 +1,12 @@
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+class Server
+{
+private:
+
+public:
+    
+};
+
+#endif SERVER_HPP_
